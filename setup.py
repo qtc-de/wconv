10ede323c512c4948eaa491d0969589afc76428a
TNeitzel <qtc_de@outlook.com>
2020-08-08 06:25:00 +0200

    Fix error in setup.py
    The setup.py accidently still used the find_packages() directive. This
    caused the 'tests' folder to be installed as a package.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    packages=find_packages(),
+    packages=['wconv'],
     package_data={
                         name: [
                             'resources/*',
